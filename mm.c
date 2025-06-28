5e8ac4ed0b25fc99814fcad39bd615f65664b5bf
H2py <ahdosj@gmail.com>
2025-06-29 00:18:34 +0900

    Add to debuger

--- mm.c
+++ mm.c
@@ -51,14 +51,14 @@ team_t team = {
 #define GET(p) (*(unsigned *)(p))
 #define PUT(p, val) (*(unsigned *)(p) = (val))
 
-#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_SIZE(p) (GET(p) & ~0xf)
 #define GET_ALLOC(p) (GET(p) & 0x1)
 
 #define HDRP(bp) ((char *)(bp) - WSIZE)
 #define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
 
-#define NEXT_BRKP (char *)(bp) + GET_SIZE(HDRP(bp))
-#define PREV_BRKP (char *)(bp) - GET_SIZE(HDRP(bp) - WSIZE)
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)))
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE((char *)(bp) - DSIZE))
 
 static char* heap_listp = NULL;
 static void* extend_heap(size_t words);
